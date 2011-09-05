b6959e72395fbe23c3325514eca427dbcc6349ab
Radek Novacek <rnovacek@redhat.com>
2011-09-05 12:29:08 +0200

    Add missing import sys

--- log.py
+++ log.py
@@ -22,6 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import logging
 import logging.handlers
 import os
+import sys
 
 def init_logger():
     logging.getLogger("rhsm-app").addHandler(_get_handler())

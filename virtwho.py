be8c2fa991efeb5e0a1c34cb2dbfe4ab7fe87685
Christopher Snyder <csnyder@redhat.com>
2015-07-02 10:26:36 -0400

    Ensures jobs are removed after being executed

--- virtwho.py
+++ virtwho.py
@@ -127,9 +127,10 @@ class VirtWho(object):
     def runJobs(self):
         if not self.jobs:
             return
-        for job in self.jobs:
+        while self.jobs:
+            job = self.jobs.pop(0)
             if hasattr(self, job.target):
-                self.logger.debug('Running method "%s" % job.target')
+                self.logger.debug('Running method "%s"' % job.target)
                 getattr(self, job.target)(*job.args)
             else:
                 self.logger.debug('VirtWho has no method "%s"' % job.target)

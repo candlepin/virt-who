544d54fe6bd84b66fdbb23f726d15147a13e1b72
Christopher Snyder <csnyder@redhat.com>
2015-07-31 12:36:09 -0400

    Adds QueueLogger Tests and FileHandler tests

--- tests/test_log.py
+++ tests/test_log.py
@@ -83,3 +83,64 @@ class TestLog(TestBase):
         self.assertTrue(len(test_logger.handlers) == 1)
         self.assertTrue(len(queueLogger.logger.handlers) == 1)
         getFileHandler.assert_called_with(test_logger.name, config.log_file, config.log_dir)
+
+    @patch('os.path.isdir')
+    def test_get_file_handler_defaults(self, isdir):
+        isdir.return_value = True
+        filtername = 'virtwho.test'
+        fileHandler = log.getFileHandler(filtername)
+        self.assertTrue(fileHandler.baseFilename == '%s/%s' % (log.DEFAULT_LOG_DIR, log.DEFAULT_LOG_FILE))
+        self.assertTrue(len(fileHandler.filters) == 1)
+        self.assertTrue(fileHandler.filters[0].name == filtername)
+
+
+    @patch('os.path.isdir')
+    def test_get_file_handler(self, isdir):
+        # Monkey patching the _open function to ensure we don't try to access a
+        # fake file
+        real_open = logging.FileHandler._open
+        logging.FileHandler._open = Mock()
+        logging.FileHandler._open.return_value = None
+
+        # Ensure we don't try to make a directory
+        isdir.return_value  = True
+        filtername = 'virtwho.test'
+        log_file = 'test.log'
+        log_dir = '/nonexistant/'
+
+        fileHandler = log.getFileHandler(filtername,
+                                         log_file,
+                                         log_dir)
+        self.assertTrue(fileHandler.baseFilename == log_dir + log_file)
+
+        logging.FileHandler._open = real_open
+
+
+class TestQueueLogger(TestBase):
+
+    @patch('multiprocessing.queues.Queue')
+    @patch('logging.getLogger')
+    def test_queue_logger(self, getLogger, queue):
+        fake_queue = sentinel.queue
+        name = sentinel.name
+        logger = sentinel.logger
+        queue.return_value = fake_queue
+        getLogger.return_value = logger
+        queueLogger = log.QueueLogger(name)
+        getLogger.assert_called_with(name)
+
+        self.assertTrue(isinstance(queueLogger._logging_thread,
+                                   threading.Thread))
+        thread = queueLogger._logging_thread
+        self.assertTrue(thread.__dict__['_Thread__target'] == log.QueueLogger._log)
+        self.assertTrue(thread.__dict__['_Thread__args'] == (logger, fake_queue))
+        self.assertTrue(queueLogger.queue == fake_queue)
+        self.assertTrue(queueLogger.logger == logger)
+        self.assertTrue(queueLogger.name == name)
+
+    def test_log_quits_on_none(self):
+        # This test will hang if the method fails to exit
+        queue = Queue()
+        queue.put(None)
+        logger = Mock()
+        log.QueueLogger._log(logger, queue)

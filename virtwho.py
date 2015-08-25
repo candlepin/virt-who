ae319d790f2f3091ccf7c73486a17c029409658e
Radek Novacek <rnovacek@redhat.com>
2015-08-25 18:48:55 +0200

    Clear obsolete reports before sending them
    When communication with subscription manager is slow, multiple updates
    from virt backend can arrive. We don't want to send old data, so we drop
    all reports are superseded by new reports.

--- virtwho.py
+++ virtwho.py
@@ -294,7 +294,7 @@ class VirtWho(object):
 
         # Queue for getting events from virt backends
         if self.queue is None:
-            self.queue = Queue(len(self.configManager.configs))
+            self.queue = Queue()
 
         # Run the virtualization backends
         self.virts = []
@@ -324,9 +324,19 @@ class VirtWho(object):
                 report = self.queue.get(block=True, timeout=self.queue_timeout)
             except Empty:
                 report = None
-                pass
             except IOError:
                 continue
+
+            # Read all the reports from the queue in order to remove obsoleted
+            # reports from same virt
+            reports = [report]
+            while True:
+                try:
+                    report = self.queue.get(block=False)
+                    reports.append(report)
+                except Empty:
+                    break
+            reports = self._remove_obsolete(reports)
 
             try:
                 # Run all jobs that have been queued as a result of sending last
@@ -340,8 +350,7 @@ class VirtWho(object):
                 pass
 
             report_sent = None
-            if report is not None:
-
+            for report in reports:
                 if report == "exit":
                     break
                 if report == "reload":
@@ -437,6 +446,20 @@ class VirtWho(object):
             virt = Virt.fromConfig(logger, config)
             mapping[config.name or 'none'] = self._readGuests(virt)
         return mapping
+
+    def _remove_obsolete(self, reports):
+        reports_dict = OrderedDict()
+        for report in reports:
+            if report is None:
+                continue
+
+            if report in ['exit', 'reload']:
+                # Throw away all other reports when report is 'exit' or 'reload'
+                return [report]
+
+            reports_dict[report.config.name] = report
+
+        return reports_dict.values()
 
 
 def exceptionCheck(e):

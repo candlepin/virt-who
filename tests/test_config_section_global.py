040c7dfff2a2f2b03cc6d4d878531a7709c4c8ac
Christopher Snyder <csnyder@redhat.com>
2017-10-30 10:48:42 -0400

    Update config_section tests to be more DRY

--- tests/test_config_section_global.py
+++ tests/test_config_section_global.py
@@ -42,6 +42,9 @@ class TestGlobalConfigSection(TestBase):
     Test base for testing class GlobalSection
     """
 
+    def setUp(self):
+        self.init_global_config_section()
+
     def init_global_config_section(self):
         """
         Method executed before each unit test
@@ -56,37 +59,33 @@ class TestGlobalConfigSection(TestBase):
         """
         Test validating of correct bool value 
         """
-        self.init_global_config_section()
         result = self.global_config._validate_str_to_bool('debug')
         self.assertIsNone(result)
         value = self.global_config.get('debug')
-        self.assertEqual(value, True)
+        self.assertIs(value, True)
 
     def test_validate_oneshot_value_string_true(self):
         """
         Test validating of correct bool value stored as string ('true') 
         """
-        self.init_global_config_section()
         result = self.global_config._validate_str_to_bool('oneshot')
         self.assertIsNone(result)
         value = self.global_config.get('oneshot')
-        self.assertEqual(value, True)
+        self.assertIs(value, True)
 
     def test_validate_print_value_string_false(self):
         """
         Test validating of correct bool value stored as string ('false') 
         """
-        self.init_global_config_section()
         result = self.global_config._validate_str_to_bool('print')
         self.assertIsNone(result)
         value = self.global_config.get('print')
-        self.assertEqual(value, False)
+        self.assertIs(value, False)
 
     def test_validate_wrong_bool_value(self):
         """
         Test validating of wrong string representing bool ('nein') 
         """
-        self.init_global_config_section()
         self.assertRaises(ValueError, str_to_bool, self.global_config['wrong_bool_value'])
         result = self.global_config._validate_str_to_bool('wrong_bool_value')
         self.assertIsNotNone(result)
@@ -96,7 +95,6 @@ class TestGlobalConfigSection(TestBase):
         """
         Test validation of non-existing key 
         """
-        self.init_global_config_section()
         result = self.global_config._validate_str_to_bool('does_not_exist')
         self.assertIsNotNone(result)
         self.assertIsInstance(result, tuple)
@@ -105,7 +103,6 @@ class TestGlobalConfigSection(TestBase):
         """
         Test validation of correct string value 
         """
-        self.init_global_config_section()
         result = self.global_config._validate_non_empty_string('log_file')
         self.assertIsNone(result)
 
@@ -113,7 +110,6 @@ class TestGlobalConfigSection(TestBase):
         """
         Test validation of time interval
         """
-        self.init_global_config_section()
         result = self.global_config._validate_interval('interval')
         self.assertIsNone(result)
 
@@ -121,19 +117,17 @@ class TestGlobalConfigSection(TestBase):
         """
         Test validation of wrong time interval
         """
-        self.init_global_config_section()
         self.global_config['interval'] = '10'
         self.global_config.validate()
         interval = self.global_config.get('interval')
         # The behavior of this has changed. We now replace any invalid value with the default
         # No special cases
-        self.assertEqual(interval, DefaultInterval)
+        self.assertIs(interval, DefaultInterval)
 
     def test_validate_missing_interval(self):
         """
         Test validation of wrong time interval
         """
-        self.init_global_config_section()
         self.global_config = GlobalSection('global', None)
         for key, value in GLOBAL_SECTION_VALUES.items():
             if key != 'interval':
@@ -141,13 +135,12 @@ class TestGlobalConfigSection(TestBase):
         result = self.global_config._validate_interval('interval')
         self.assertIsNotNone(result)
         interval = self.global_config['interval']
-        self.assertEqual(interval, DefaultInterval)
+        self.assertIs(interval, DefaultInterval)
 
     def test_validate_configs(self):
         """
         Test validation of configs (list of paths to config files)
         """
-        self.init_global_config_section()
         result = self.global_config._validate_configs()
         self.assertIsNone(result)
 
@@ -155,7 +148,6 @@ class TestGlobalConfigSection(TestBase):
         """
         Test validation of all config values 
         """
-        self.init_global_config_section()
         validate_messages = self.global_config.validate()
         # TODO: use following expected messages, when format of warning/error messages will be settled down.
         # expected_results = [
@@ -176,7 +168,6 @@ class TestGlobalConfigSection(TestBase):
         Test the validate method will set default value, when an option
         is missing and default value exist in DEFAULT
         """
-        self.init_global_config_section()
         self.global_config.validate()
         default_value = self.global_config.get('log_dir')
-        self.assertEqual(default_value, DEFAULT_LOG_DIR)
+        self.assertIs(default_value, DEFAULT_LOG_DIR)

c0c97ecd488428255594dc23f864f48e746cd535
Kevin Howell <khowell@redhat.com>
2018-01-19 13:33:00 -0500

    1353119: Add JSON-RPC support for VDSM
    Had to fix xmlrpc on modern Python; M2Crypto appears to be broken in F27 at least...

---
+++ virtwho/virt/vdsm/jsonrpc.py
@@ -0,0 +1,149 @@
+# Module for JSON-RPC over STOMP, part of virt-who
+#
+# Copyright (C) 2018 Kevin Howell <khowell@redhat.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""
+Minimal implementation of the JSON-RPC specification (http://www.jsonrpc.org/specification) over STOMP to enable a
+client for VDSM.
+
+There are better developed JSON-RPC libraries for Python available (such as jsonrpclib), but for RHEL/CentOS, at the
+time of writing, only available in EPEL.
+"""
+
+from __future__ import unicode_literals
+
+import json
+import ssl
+import uuid
+
+from virtwho.virt.vdsm.stomp import StompClient
+
+
+class JsonRpcClient(object):
+    """JSON-RPC 2.0 over STOMP client"""
+
+    def __init__(self, host, port, ssl_context=None, timeout=None):
+        """Create a JSON-RPC over STOMP client.
+
+        :param host: STOMP server hostname.
+        :param port: STOMP server port.
+        :param ssl_context: SSL context if using TLS or None.
+        :param timeout: timeout for blocking operations.
+        """
+        self.id = unicode(uuid.uuid4())
+        self.stomp = StompClient(host, port, ssl_context=ssl_context, timeout=timeout)
+
+    def connect(self):
+        """Connect to the STOMP server."""
+        self.stomp.connect()
+        self.stomp.subscribe(self.id)
+
+    def close(self):
+        """Close the underlying connection."""
+        self.stomp.close()
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.close()
+
+    def call(self, method, **params):
+        """Call a method via JSON-RPC.
+
+        :param method: name of method.
+        :param params: parameters for the method.
+        :return: the result as a dict if successful.
+        :raises: RuntimeError if the result is an error or cannot be decoded.
+        """
+        data_object = {
+            'jsonrpc': '2.0',
+            'method': method,
+            'id': unicode(uuid.uuid4())
+        }
+        if params:
+            data_object['params'] = {}
+            for key, value in params.items():
+                data_object['params'][key] = value
+        data = json.dumps(data_object).encode('utf-8')
+        self.stomp.send('SEND', {
+            'destination': 'jms.topic.vdsm_requests',
+            'content-length': len(data),
+            'reply-to': self.id,
+        }, data)
+        response = self.stomp.receive()
+        if response.command == 'ERROR':
+            raise RuntimeError(response.body.decode('utf-8'))
+        response_json = json.loads(response.body.decode('utf-8'))
+        if 'error' in response_json:
+            # TODO log verbose error object?
+            raise RuntimeError(response_json['error']['message'])
+        return response_json['result']
+
+
+def main():
+    """Utility for one-off calls to VDSM via JSON-RPC over STOMP.
+
+    Example invocation:
+
+    python -m virtwho.virt.vdsm.jsonrpc
+        --cert /etc/pki/vdsm/certs/vdsmcert.pem
+        --key /etc/pki/vdsm/keys/vdsmkey.pem
+        $HOSTNAME 54321
+        Host.getVMList onlyUUID=False
+    """
+    import argparse
+
+    parser = argparse.ArgumentParser(description='vdsm over jsonrpc utility')
+
+    parser.add_argument('--key', help='path to key pem file')
+    parser.add_argument('--cert', help='path to cert pem file')
+    parser.add_argument('host', help='hostname')
+    parser.add_argument('port', help='port')
+    parser.add_argument('method', help='method to call via JSON-RPC')
+    parser.add_argument('args', help='arguments for the call with format $key=$value', nargs='*')
+    args = parser.parse_args()
+
+    ssl_enabled = 'cert' in args and 'key' in args
+    if ('key' in args or 'cert' in args) and not ssl_enabled:
+        raise ValueError('Must provide both --cert and --key for SSL')
+
+    ssl_context = None
+    if ssl_enabled:
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        ssl_context.verify_mode = ssl.CERT_NONE
+        ssl_context.load_cert_chain(args.cert, args.key)
+
+    # parse method args
+    method_args = {}
+    for arg in args.args:
+        key, value = arg.split('=')
+        key = key.strip()
+        value = value.strip()
+        if value.lower() == 'true':
+            value = True
+        if value.lower() == 'false':
+            value = False
+        method_args[key] = value
+
+    with JsonRpcClient(args.host, args.port, ssl_context=ssl_context) as jsonrpc_client:
+        print(json.dumps(jsonrpc_client.call(args.method, **method_args), indent=True))
+
+
+if __name__ == '__main__':
+    main()
--- virtwho/virt/vdsm/vdsm.py
+++ virtwho/virt/vdsm/vdsm.py
@@ -24,10 +24,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import re
 import xmlrpclib
 from ConfigParser import SafeConfigParser, NoSectionError, NoOptionError
+import ssl
 import subprocess
 
 from virtwho.virt import Virt, Guest
 from virtwho.config import VirtConfigSection
+from virtwho.virt.vdsm.jsonrpc import JsonRpcClient
 
 
 class VdsmError(Exception):
@@ -47,6 +49,8 @@ VDSM_STATE_TO_GUEST_STATE = {
     'WaitForLaunch': Guest.STATE_SHUTOFF,
     'Powering up': Guest.STATE_SHUTOFF
 }
+
+VDSM_JSONRPC_TIMEOUT = 300  # 5 minutes, borrowed from esx MAX_WAIT_TIME
 
 
 class VdsmConfigSection(VirtConfigSection):
@@ -75,7 +79,8 @@ class Vdsm(Virt):
                                    interval=interval,
                                    oneshot=oneshot)
         self._read_config("/etc/vdsm/vdsm.conf")
-        self.server = None
+        self.xmlrpc_client = None
+        self.jsonrpc_client = None
 
     def isHypervisor(self):
         return False
@@ -107,46 +112,120 @@ class Vdsm(Virt):
         out, err = p.communicate()
         if p.returncode != 0:
             return '0'
-        return re.search('/CN=([^/$\n]+)', out).group(1)
+        return re.search('CN *= *([^,/$\n]+)', out).group(1)
 
-    def _secure_connect(self):
-        addr = self._get_local_vds_name(self.trust_store_path)
+    def _get_addr(self):
+        return self._get_local_vds_name(self.trust_store_path)
 
-        from M2Crypto.m2xmlrpclib import SSL_Transport
-        from M2Crypto import SSL
+    @staticmethod
+    def _need_m2crypto():
+        """Determine whether M2Crypto usage is necessary.
 
+        If the python version supports SSL contexts and allows specifying them in xmlrpc clients, we do not need
+        M2Crypto.
+        """
+        if not hasattr(ssl, 'create_default_context'):
+            return False
+        try:
+            xmlrpclib.ServerProxy('https://localhost', context=ssl.create_default_context())
+            return True
+        except TypeError:
+            return False
+
+    def _create_ssl_context(self):
         key_file = self.trust_store_path + '/keys/vdsmkey.pem'
         cert_file = self.trust_store_path + '/certs/vdsmcert.pem'
         ca_cert = self.trust_store_path + '/certs/cacert.pem'
 
-        ctx = SSL.Context()
+        if self._need_m2crypto():
+            ctx = ssl.create_default_context(capath=ca_cert)
+            ctx.load_cert_chain(cert_file, key_file)
+            ctx.check_hostname = False
+            ctx.verify_mode = ssl.CERT_NONE
+        else:
+            from M2Crypto import SSL
 
-        ctx.set_verify(SSL.verify_peer | SSL.verify_fail_if_no_peer_cert, 16)
-        ctx.load_verify_locations(ca_cert)
-        ctx.load_cert(cert_file, key_file)
+            ctx = SSL.Context()
 
-        return xmlrpclib.Server('https://%s:%s' % (addr, self.management_port), SSL_Transport(ctx))
+            ctx.set_verify(SSL.verify_peer | SSL.verify_fail_if_no_peer_cert, 16)
+            ctx.load_verify_locations(ca_cert)
+            ctx.load_cert(cert_file, key_file)
+        return ctx
 
     def connect(self):
         if self.trust_store_path:
-            try:
-                self.server = self._secure_connect()
+            ssl_context = self._create_ssl_context()
+            addr = self._get_local_vds_name(self.trust_store_path)
+            client = self._jsonrpc(addr, ssl_context) or self._xmlrpc(addr, ssl_context)
+            if client:
                 return
-            except Exception as e:
-                self.logger.exception(e)
-        # Try http version if ssl is off or fails
-        self.server = xmlrpclib.Server("http://localhost:%s" % self.management_port)
+
+        # Try http versions if ssl is off or fails
+        self._jsonrpc('localhost') or self._xmlrpc('localhost')
+
+    def _jsonrpc(self, addr, ssl_context=None):
+        try:
+            jsonrpc_client = JsonRpcClient(addr, self.management_port, ssl_context, timeout=VDSM_JSONRPC_TIMEOUT)
+            jsonrpc_client.connect()
+            self.jsonrpc_client = jsonrpc_client
+            return True
+        except IOError:
+            if ssl_context:
+                description = 'JSON-RPC with SSL'
+            else:
+                description = 'JSON-RPC'
+            self.logger.warning('Unable to connect via %s' % description, exc_info=True)
+        return False
+
+    def _xmlrpc(self, addr, ssl_context=None):
+        try:
+            transport = None
+            if ssl_context:
+                if self._need_m2crypto():
+                    transport = xmlrpclib.SafeTransport(context=ssl_context)
+                else:
+                    from M2Crypto.m2xmlrpclib import SSL_Transport
+                    transport = SSL_Transport(ssl_context)
+                uri = 'https://%s:%s' % (addr, self.management_port)
+            else:
+                uri = 'http://%s:%s' % (addr, self.management_port)
+            xmlrpc_client = xmlrpclib.ServerProxy(uri, transport)
+            xmlrpc_client.system.listMethods()
+            self.xmlrpc_client = xmlrpc_client
+            return True
+        except Exception:  # NOTE: unfortunately there are many ways to fail, so the exception clause is broad
+            if ssl_context:
+                description = 'XML-RPC with SSL'
+            else:
+                description = 'XML-RPC'
+            self.logger.warning('Unable to connect via %s' % description, exc_info=True)
+        return False
 
     def prepare(self):
         self.connect()
 
-    def listDomains(self):
-        domains = []
-        response = self.server.list(True)
+    def _get_vm_list_xmlrpc(self):
+        response = self.xmlrpc_client.list(True)
         if response['status']['code'] != 0:
             self.logger.error("Unable to list virtual machines: %s", response['status']['message'])
         else:
-            for vm in response['vmList']:
+            return response['vmList']
+
+    def _get_vm_list_jsonrpc(self):
+        try:
+            return self.jsonrpc_client.call('Host.getVMList', onlyUUID=False)
+        except (IOError, RuntimeError):
+            self.logger.exception("Unable to list virtual machines")
+
+    def listDomains(self):
+        domains = []
+        vm_list = None
+        if self.jsonrpc_client is not None:
+            vm_list = self._get_vm_list_jsonrpc()
+        elif self.xmlrpc_client is not None:
+            vm_list = self._get_vm_list_xmlrpc()
+        if vm_list:
+            for vm in vm_list:
                 status = VDSM_STATE_TO_GUEST_STATE.get(vm['status'], Guest.STATE_UNKNOWN)
                 domains.append(Guest(vm['vmId'], self.CONFIG_TYPE, status))
         return domains

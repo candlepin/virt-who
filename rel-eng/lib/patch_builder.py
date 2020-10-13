ff07d2083234abf6b190251a4f2b7e55ab0755ac
William Poteat <wpoteat@redhat.com>
2020-10-13 11:53:04 -0400

    Correction in patch builder for directory location

--- rel-eng/lib/patch_builder.py
+++ rel-eng/lib/patch_builder.py
@@ -15,6 +15,6 @@ class PatchBuilder(DistributionBuilder):
         retval = DistributionBuilder.tgz(self)
         self.sources.append(os.path.join(self.rpmbuild_basedir, 'build-rpm-no-ahv.patch'))
         self.artifacts.append(os.path.join(self.rpmbuild_basedir, 'build-rpm-no-ahv.patch'))
-        subprocess.check_call("cp %s %s/" % ('build-rpm-no-ahv.patch', self.rpmbuild_sourcedir), shell=True)
+        subprocess.check_call("cp %s %s/" % ('build-rpm-no-ahv.patch', self.rpmbuild_basedir), shell=True)
 
         return retval
